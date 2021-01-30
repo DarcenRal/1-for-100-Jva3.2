65177d31658fe18c02648b138937bc7c827bf7a1
DarcenRal <75911924+DarcenRal@users.noreply.github.com>
2021-01-30 12:19:07 +0300

    Added Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int Top_up_amount = 1_500;
+        int topUpAmount = 1500;
         int balans = 100;
         int bonus;
         int every =100;
-        if (Top_up_amount >= 1_000) {
-            bonus = (Top_up_amount + balans) / every;
+        if (topUpAmount > 1_000) {
+            bonus = (topUpAmount + balans) / every;
         } else {
             bonus = 0;
         }
-        int total_balance = Top_up_amount + balans + bonus;
-        System.out.println(total_balance);
+        int totalBalance = topUpAmount + balans + bonus;
+        System.out.println(totalBalance);
     }
 }
